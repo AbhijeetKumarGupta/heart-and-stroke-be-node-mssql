12a6fb93cef20fb6e18d259e44df8e802446c64a
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2024-12-26 16:00:41 +0530

    fix: cors issue

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require('dotenv').config(); 
 
 const express = require('express');
+const cors = require('cors');
 const { connectDB } = require('./config/database');
 const surveyRoutes = require('./routes/surveyRoutes');
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use(express.json());
 
